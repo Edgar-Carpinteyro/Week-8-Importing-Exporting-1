4d5f44e8bb8ad5442ac65ac02bd40400fbb674b2
Edgar-Carpinteyro <carpinteyroe07@gmail.com>
2023-11-30 19:20:54 -0800

    first commit

---
+++ calculator.js
@@ -0,0 +1,16 @@
+import { add } from "./add.js";
+import { subtract } from "./subtract.js";
+import { multiply } from "./multiply.js";
+import { divide } from "./divide.js";
+
+const result = add(5, 3);
+console.log(result);
+
+const result2 = subtract(5, 3);
+console.log(result2);
+
+const result3 = multiply(5, 3);
+console.log(result3);
+
+const result4 = divide(5, 3);
+console.log(result4);
